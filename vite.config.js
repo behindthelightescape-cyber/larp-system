f7a501bc2c1a77e23bcd5c11c33bfbe0e8a2e0c3
Boss Joe <boss@larp.com>
2026-03-07 01:36:15 +0800

    202603070120

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,17 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  optimizeDeps: {
+    include: ['@line/liff']
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('@line/liff')) return 'liff'
+          if (id.includes('node_modules')) return 'vendor'
+        }
+      }
+    }
+  }
 })
